a34b66711fffc6c9a6062c94f0f9ba7db1a0cfbc
Matthew Yang <matthewyang434@gmail.com>
2023-09-16 20:26:32 -0500

    create constant values

---
+++ index.js
@@ -0,0 +1,4 @@
+const pokemonInput = document.getElementById('pokemon-input');
+const searchBtn = document.getElementById('search-btn');
+const pokemonImg = document.getElementById('pokemon-img');
+const pokemonName = document.getElementById('pokemon-name');
