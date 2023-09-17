9bbebd9414d4a0d4da73b7de28bed309065641e6
Matthew Yang <matthewyang434@gmail.com>
2023-09-16 20:29:52 -0500

    adds event listener for search button

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const pokemonInput = document.getElementById('pokemon-input');
 const searchBtn = document.getElementById('search-btn');
 const pokemonImg = document.getElementById('pokemon-img');
 const pokemonName = document.getElementById('pokemon-name');
+
+searchBtn.addEventListener('click', () => {
+  const pokemonQuery = pokemonInput.value.toLowerCase();
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonQuery}`)
+      .then(response => response.json())
+      .then(data => {
+          pokemonImg.src = data.sprites.front_default;
+          pokemonName.textContent = data.name;
+      })
+      .catch(error => {
+          alert('Could not find Pokémon. Please try again.');
+      });
+});
